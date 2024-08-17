8166ab2bc456c56a6d8949ea05f9040df562d649
rikkhanna <rishabhkhanna726@gmail.com>
2024-08-17 23:01:28 +0530

    test

--- terraform/nginx/main.tf
+++ terraform/nginx/main.tf
@@ -102,6 +102,6 @@ resource "google_compute_instance" "nginx" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook  -i ${self.network_interface[0].access_config[0].nat_ip}, --private-key ansible-key  playbook.yml"
+    command = "ansible-playbook  -i ${self.network_interface[0].access_config[0].nat_ip}, -u ansible --private-key ansible-key  playbook.yml"
   }
 }
